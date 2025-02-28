4aeb828b7a323d303b0e14611db1eca25ee2d689
Ali Saleh <alisaleh06@outlook.com.au>
2025-02-28 19:13:11 +1100

    Chat bug fixed

--- src/app/api/chat/[id]/message/route.js
+++ src/app/api/chat/[id]/message/route.js
@@ -1,28 +1,29 @@
-import { MongoClient } from "mongodb";
-import { NextResponse } from "next/server";
+import { MongoClient } from 'mongodb';
+import { NextResponse } from 'next/server';
 
 const uri = process.env.NEXT_PUBLIC_MONGODB_URI;
 const client = new MongoClient(uri);
 
 export async function POST(request, { params }) {
-	try {
-		const { messageData: userMessage, aiResponse } = await request.json();
-		const chatId = await params.id;
+    try {
+        const { messageData: userMessage, aiResponse } = await request.json();
+        const param = await params;
+        const chatId = param.id;
 
-		const db = client.db("Whooosh");
-		const messages = db.collection("messages");
+        const db = client.db('Whooosh');
+        const messages = db.collection('messages');
 
-		const newMessage = {
-			userMessage,
-			aiResponse,
-			chatId: chatId,
-			timestamp: new Date(),
-		};
+        const newMessage = {
+            userMessage,
+            aiResponse,
+            chatId: chatId,
+            timestamp: new Date(),
+        };
 
-		await messages.insertOne(newMessage);
-		return NextResponse.json({ success: true }, { status: 201 });
-	} catch (err) {
-		console.log(err);
-		return NextResponse.json({ error: err }, { status: 500 });
-	}
+        await messages.insertOne(newMessage);
+        return NextResponse.json({ success: true }, { status: 201 });
+    } catch (err) {
+        console.log(err);
+        return NextResponse.json({ error: err }, { status: 500 });
+    }
 }
