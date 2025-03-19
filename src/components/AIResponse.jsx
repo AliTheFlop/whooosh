ac60b938229d56383b613aad0f9d165945eb3c13
Ali Saleh <alisaleh06@outlook.com.au>
2025-03-19 12:24:41 +1100

    Updated and styled model selector

--- src/components/AIResponse.jsx
+++ src/components/AIResponse.jsx
@@ -2,24 +2,41 @@
 
 import React, { useEffect } from "react";
 import Prism from "prismjs";
-import "prismjs/themes/prism-tomorrow.css";
-
-import "prismjs/components/prism-core";
-import "prismjs/components/prism-markup";
-import "prismjs/components/prism-clike";
-
-// Specific language support
+import "prismjs/components/prism-clike"; // Import core language definitions
 import "prismjs/components/prism-javascript";
-import "prismjs/components/prism-jsx"; // Add this for JSX support
-import "prismjs/components/prism-python";
-import "prismjs/components/prism-bash";
+import "prismjs/components/prism-jsx";
+import "prismjs/components/prism-tsx";
+import "prismjs/components/prism-typescript";
+import "prismjs/components/prism-markup";
 import "prismjs/components/prism-css";
+import "prismjs/components/prism-bash";
+import "prismjs/components/prism-c";
+import "prismjs/components/prism-cpp";
+import "prismjs/components/prism-csharp";
+import "prismjs/components/prism-java";
+import "prismjs/components/prism-json";
+import "prismjs/components/prism-json5";
+import "prismjs/components/prism-jsonp";
+import "prismjs/components/prism-markdown";
+import "prismjs/components/prism-markup-templating";
+import "prismjs/components/prism-php";
+import "prismjs/components/prism-sql";
+import "prismjs/components/prism-plsql";
+import "prismjs/components/prism-python";
+import "prismjs/plugins/line-numbers/prism-line-numbers";
+import "prismjs/plugins/line-numbers/prism-line-numbers.css";
+import "prismjs/plugins/toolbar/prism-toolbar";
+import "prismjs/plugins/toolbar/prism-toolbar.css";
+import "prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard";
 
 export default function AIResponse({ content }) {
 	useEffect(() => {
-		Prism.highlightAll();
+		try {
+			Prism.highlightAll();
+		} catch (error) {
+			console.error(error);
+		}
 	});
-
 	return (
 		<div className="my-4">
 			<div className="rounded-2xl bg-white px-4 py-3">
